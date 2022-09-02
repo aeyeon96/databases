b3e2a35bce987b84ba1ee749cb32b157bd4b0b2d
aeyeon96 <dldodus6487@gmail.com>
2022-09-02 10:15:22 +0900

    [update]

---
+++ Programmers/LEVEL02/join.sql
@@ -0,0 +1,6 @@
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID)
+INNER JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID) 
+INNER JOIN Products ON OrderDetails.ProductID = Products.ProductID);
